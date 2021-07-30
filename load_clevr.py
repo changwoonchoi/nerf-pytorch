77070118a4e07408130d211cf0686d4e5d790ffc
ccw <zzzmaster@snu.ac.kr>
2021-07-30 13:31:11 +0900

    change space to tab

--- load_clevr.py
+++ load_clevr.py
@@ -9,148 +9,148 @@ from utils import color2label
 
 
 trans_t = lambda t : torch.Tensor([
-    [1,0,0,0],
-    [0,1,0,0],
-    [0,0,1,t],
-    [0,0,0,1]]).float()
+	[1,0,0,0],
+	[0,1,0,0],
+	[0,0,1,t],
+	[0,0,0,1]]).float()
 rot_phi = lambda phi : torch.Tensor([
-    [1,0,0,0],
-    [0,np.cos(phi),-np.sin(phi),0],
-    [0,np.sin(phi), np.cos(phi),0],
-    [0,0,0,1]]).float()
+	[1,0,0,0],
+	[0,np.cos(phi),-np.sin(phi),0],
+	[0,np.sin(phi), np.cos(phi),0],
+	[0,0,0,1]]).float()
 
 rot_theta = lambda th : torch.Tensor([
-    [np.cos(th),0,-np.sin(th),0],
-    [0,1,0,0],
-    [np.sin(th),0, np.cos(th),0],
-    [0,0,0,1]]).float()
+	[np.cos(th),0,-np.sin(th),0],
+	[0,1,0,0],
+	[np.sin(th),0, np.cos(th),0],
+	[0,0,0,1]]).float()
 
 
 def pose_spherical(theta, phi, radius):
-    c2w = trans_t(radius)
-    c2w = rot_phi(phi/180.*np.pi) @ c2w
-    c2w = rot_theta(theta/180.*np.pi) @ c2w
-    c2w = torch.Tensor(np.array([[-1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]])) @ c2w
-    return c2w
+	c2w = trans_t(radius)
+	c2w = rot_phi(phi/180.*np.pi) @ c2w
+	c2w = rot_theta(theta/180.*np.pi) @ c2w
+	c2w = torch.Tensor(np.array([[-1,0,0,0],[0,0,1,0],[0,1,0,0],[0,0,0,1]])) @ c2w
+	return c2w
 
 
 def load_clevr_data(basedir, half_res=False, testskip=1):
-    splits = ['train', 'val', 'test']
-    metas = {}
-    for s in splits:
-        with open(os.path.join(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
-            metas[s] = json.load(fp)
-    
-    all_imgs = []
-    all_poses = []
-    counts = [0]
-    for s in splits:
-        meta = metas[s]
-        imgs = []
-        poses = []
-        if s=='train' or testskip==0:
-            skip = 1
-        else:
-            skip = testskip
-            
-        for frame in meta['frames'][::skip]:
-            fname = os.path.join(basedir, frame['file_path'])
-            imgs.append(imageio.imread(fname)[..., :3])
-            poses.append(np.array(frame['transform_matrix']))
-        imgs = (np.array(imgs) / 255.).astype(np.float32)  # keep all 4 channels (RGBA)
-        poses = np.array(poses).astype(np.float32)
-        counts.append(counts[-1] + imgs.shape[0])
-        all_imgs.append(imgs)
-        all_poses.append(poses)
-    
-    i_split = [np.arange(counts[i], counts[i+1]) for i in range(3)]
-    
-    imgs = np.concatenate(all_imgs, 0)
-    poses = np.concatenate(all_poses, 0)
-    
-    H, W = imgs[0].shape[:2]
-    camera_angle_x = float(meta['camera_angle_x'])
-    focal = .5 * W / np.tan(.5 * camera_angle_x)
-    
-    render_poses = torch.stack([pose_spherical(angle, -30.0, 11.0) for angle in np.linspace(-180,180,40+1)[:-1]], 0)
-    
-    if half_res:
-        H = H//2
-        W = W//2
-        focal = focal/2.
+	splits = ['train', 'val', 'test']
+	metas = {}
+	for s in splits:
+		with open(os.path.join(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
+			metas[s] = json.load(fp)
+	
+	all_imgs = []
+	all_poses = []
+	counts = [0]
+	for s in splits:
+		meta = metas[s]
+		imgs = []
+		poses = []
+		if s=='train' or testskip==0:
+			skip = 1
+		else:
+			skip = testskip
+			
+		for frame in meta['frames'][::skip]:
+			fname = os.path.join(basedir, frame['file_path'])
+			imgs.append(imageio.imread(fname)[..., :3])
+			poses.append(np.array(frame['transform_matrix']))
+		imgs = (np.array(imgs) / 255.).astype(np.float32)  # keep all 4 channels (RGBA)
+		poses = np.array(poses).astype(np.float32)
+		counts.append(counts[-1] + imgs.shape[0])
+		all_imgs.append(imgs)
+		all_poses.append(poses)
+	
+	i_split = [np.arange(counts[i], counts[i+1]) for i in range(3)]
+	
+	imgs = np.concatenate(all_imgs, 0)
+	poses = np.concatenate(all_poses, 0)
+	
+	H, W = imgs[0].shape[:2]
+	camera_angle_x = float(meta['camera_angle_x'])
+	focal = .5 * W / np.tan(.5 * camera_angle_x)
+	
+	render_poses = torch.stack([pose_spherical(angle, -30.0, 11.0) for angle in np.linspace(-180,180,40+1)[:-1]], 0)
+	
+	if half_res:
+		H = H//2
+		W = W//2
+		focal = focal/2.
 
-        imgs_half_res = np.zeros((imgs.shape[0], H, W, 3))
-        for i, img in enumerate(imgs):
-            imgs_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
-        imgs = imgs_half_res
+		imgs_half_res = np.zeros((imgs.shape[0], H, W, 3))
+		for i, img in enumerate(imgs):
+			imgs_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
+		imgs = imgs_half_res
 
-    return imgs, poses, render_poses, [H, W, focal], i_split
+	return imgs, poses, render_poses, [H, W, focal], i_split
 
 
 def load_clevr_instance_data(basedir, half_res=False, testskip=1):
-    splits = ['train', 'val', 'test']
-    metas = {}
-    for s in splits:
-        with open(os.path.join(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
-            metas[s] = json.load(fp)
-    
-    instance_color_list = torch.from_numpy(np.loadtxt(os.path.join(basedir, 'train/instance_label_render.txt'))).to(torch.uint8)
-    instance_num = instance_color_list.shape[0]  # include background
+	splits = ['train', 'val', 'test']
+	metas = {}
+	for s in splits:
+		with open(os.path.join(basedir, 'transforms_{}.json'.format(s)), 'r') as fp:
+			metas[s] = json.load(fp)
+	
+	instance_color_list = torch.from_numpy(np.loadtxt(os.path.join(basedir, 'train/instance_label_render.txt'))).to(torch.uint8)
+	instance_num = instance_color_list.shape[0]  # include background
 
-    all_imgs = []
-    all_masks = []
-    all_poses = []
-    counts = [0]
-    for s in splits:
-        meta = metas[s]
-        imgs = []
-        masks = []
-        poses = []
-        if s=='train' or testskip==0:
-            skip = 1
-        else:
-            skip = testskip
-            
-        for frame in meta['frames'][::skip]:
-            fname = os.path.join(basedir, frame['file_path'])
-            imgs.append(imageio.imread(fname)[..., :3])
-            colored_mask = imageio.imread(os.path.join(os.path.split(fname)[0], 'mask_' + os.path.split(fname)[1]))
-            colored_mask = torch.from_numpy(colored_mask[..., :3])
-            mask = color2label(colored_mask, instance_color_list).cpu().numpy()
-            masks.append(mask)
-            poses.append(np.array(frame['transform_matrix']))
-        imgs = (np.array(imgs) / 255.).astype(np.float32)  # keep all 4 channels (RGBA)
-        poses = np.array(poses).astype(np.float32)
-        counts.append(counts[-1] + imgs.shape[0])
-        all_imgs.append(imgs)
-        all_masks.append(masks)
-        all_poses.append(poses)
-    
-    i_split = [np.arange(counts[i], counts[i+1]) for i in range(3)]
-    
-    imgs = np.concatenate(all_imgs, 0)
-    masks = np.concatenate(all_masks, 0)
-    poses = np.concatenate(all_poses, 0)
-    
-    H, W = imgs[0].shape[:2]
-    camera_angle_x = float(meta['camera_angle_x'])
-    focal = .5 * W / np.tan(.5 * camera_angle_x)
-    
-    render_poses = torch.stack([pose_spherical(angle, -30.0, 11.0) for angle in np.linspace(-180,180,40+1)[:-1]], 0)
-    
-    if half_res:
-        H = H//2
-        W = W//2
-        focal = focal/2.
+	all_imgs = []
+	all_masks = []
+	all_poses = []
+	counts = [0]
+	for s in splits:
+		meta = metas[s]
+		imgs = []
+		masks = []
+		poses = []
+		if s=='train' or testskip==0:
+			skip = 1
+		else:
+			skip = testskip
+			
+		for frame in meta['frames'][::skip]:
+			fname = os.path.join(basedir, frame['file_path'])
+			imgs.append(imageio.imread(fname)[..., :3])
+			colored_mask = imageio.imread(os.path.join(os.path.split(fname)[0], 'mask_' + os.path.split(fname)[1]))
+			colored_mask = torch.from_numpy(colored_mask[..., :3])
+			mask = color2label(colored_mask, instance_color_list).cpu().numpy()
+			masks.append(mask)
+			poses.append(np.array(frame['transform_matrix']))
+		imgs = (np.array(imgs) / 255.).astype(np.float32)  # keep all 4 channels (RGBA)
+		poses = np.array(poses).astype(np.float32)
+		counts.append(counts[-1] + imgs.shape[0])
+		all_imgs.append(imgs)
+		all_masks.append(masks)
+		all_poses.append(poses)
+	
+	i_split = [np.arange(counts[i], counts[i+1]) for i in range(3)]
+	
+	imgs = np.concatenate(all_imgs, 0)
+	masks = np.concatenate(all_masks, 0)
+	poses = np.concatenate(all_poses, 0)
+	
+	H, W = imgs[0].shape[:2]
+	camera_angle_x = float(meta['camera_angle_x'])
+	focal = .5 * W / np.tan(.5 * camera_angle_x)
+	
+	render_poses = torch.stack([pose_spherical(angle, -30.0, 11.0) for angle in np.linspace(-180,180,40+1)[:-1]], 0)
+	
+	if half_res:
+		H = H//2
+		W = W//2
+		focal = focal/2.
 
-        imgs_half_res = np.zeros((imgs.shape[0], H, W, 3))
-        for i, img in enumerate(imgs):
-            imgs_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
-        imgs = imgs_half_res
+		imgs_half_res = np.zeros((imgs.shape[0], H, W, 3))
+		for i, img in enumerate(imgs):
+			imgs_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
+		imgs = imgs_half_res
 
-        masks_half_res = np.zeros((masks.shape[0], H, W, 3))
-        for i, mask in enumerate(masks):
-            masks_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
-        masks = masks_half_res
+		masks_half_res = np.zeros((masks.shape[0], H, W, 3))
+		for i, mask in enumerate(masks):
+			masks_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
+		masks = masks_half_res
 
-    return imgs, masks, instance_num, poses, render_poses, [H, W, focal], i_split
+	return imgs, masks, instance_num, poses, render_poses, [H, W, focal], i_split
--- utils.py
+++ utils.py
@@ -1,31 +1,31 @@
 import torch
 
 def label2color(label, color_list):
-    """
-    params:
-        label: (H, W) int torch tensor
-        color_list: colors of labels
-    returns:
-        colored_mask: (H, W, 3)
-    """
-    colored_mask = torch.zeros([label.shape[0], label.shape[1], 3])
-    for i in range(int(torch.max(label).item())):
-        mask_i = label == i
-        colored_mask[mask_i] = color_list[i]
-    # TODO: need to verify
-    return colored_mask
+	"""
+	params:
+		label: (H, W) int torch tensor
+		color_list: colors of labels
+	returns:
+		colored_mask: (H, W, 3)
+	"""
+	colored_mask = torch.zeros([label.shape[0], label.shape[1], 3])
+	for i in range(int(torch.max(label).item())):
+		mask_i = label == i
+		colored_mask[mask_i] = color_list[i]
+	# TODO: need to verify
+	return colored_mask
 
 
 def color2label(colored_mask, color_list):
-    """
-    params:
-        colored_mask: (H, W, 3)
-        color_list: colors of labels
-    returns:
-        label: (H, W) torch tensor
-    """
-    label = torch.zeros([colored_mask.shape[0], colored_mask.shape[1]])
-    for i, color in enumerate(color_list):
-        mask_i = torch.sum((colored_mask.view(-1, 3) == color), dim=-1).reshape([label.shape[0], label.shape[1]]).to(torch.bool)
-        label[mask_i] = i
-    return label.to(torch.uint8)
+	"""
+	params:
+		colored_mask: (H, W, 3)
+		color_list: colors of labels
+	returns:
+		label: (H, W) torch tensor
+	"""
+	label = torch.zeros([colored_mask.shape[0], colored_mask.shape[1]])
+	for i, color in enumerate(color_list):
+		mask_i = torch.sum((colored_mask.view(-1, 3) == color), dim=-1).reshape([label.shape[0], label.shape[1]]).to(torch.bool)
+		label[mask_i] = i
+	return label.to(torch.uint8)
