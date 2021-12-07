20211207 normal calculation fix & approximate from albedo, normal, roughness gt

src/dataset/dataset_interface.py
@@ -30,9 +30,11 @@ class NerfDataset(Dataset, ABC):
 		self.masks = []
 		self.normals = []
 		self.albedos = []
+		self.roughness = []
 
 		self.load_normal = kwargs.get("load_normal", False)
 		self.load_albedo = kwargs.get("load_albedo", False)
+		self.load_roughness = kwargs.get("load_roughness", False)
 
 		self.instance_color_list = []
 		self.instance_num = 0
@@ -67,11 +69,15 @@ class NerfDataset(Dataset, ABC):
 		t = transforms.Resize(size=(self.height // resize_factor, self.width // resize_factor), antialias=True)
 		normal_temp = self.normals[i].permute((2, 0, 1))
 		albedo_temp = self.albedos[i].permute((2, 0, 1))
+		roughness_temp = self.roughness[i].permute((2,0,1))
 		normal_temp = t(normal_temp).permute((1, 2, 0))
 		albedo_temp = t(albedo_temp).permute((1, 2, 0))
+		roughness_temp = t(roughness_temp).permute((1,2,0))
+
 		return {
 			"normal": normal_temp,
-			"albedo": albedo_temp
+			"albedo": albedo_temp,
+			"roughness": roughness_temp
 		}
 
 	def get_coarse_images(self, level):
@@ -92,13 +98,10 @@ class NerfDataset(Dataset, ABC):
 
 	def get_info(self, image_index, u, v):
 		pixel_info = {}
-		#g = transforms.GaussianBlur(kernel_size=5, sigma=2)
-		#K = 4
 		t_orig = transforms.Resize(size=(self.height, self.width), antialias=True)
 
-		import matplotlib.pyplot as plt
 		image_temp = self.images[image_index].permute((2, 0, 1))
-		# image_temp = image_temp.permute((1,2,0))
+
 		sh = self.height
 		sw = self.width
 		for i in range(self.coarse_radiance_number):
@@ -107,15 +110,10 @@ class NerfDataset(Dataset, ABC):
 			sw = sw//self.coarse_resize_scale
 			t = transforms.Resize(size=(sh, sw), antialias=True)
 			image_temp = t(image_temp)
-			new_image = t_orig(image_temp).permute((1,2,0))
-			#plt.figure()
-			#plt.imshow(new_image.cpu().numpy())
+			new_image = t_orig(image_temp).permute((1, 2, 0))
 			pixel_info["rgb_%d" % (i+1)] = new_image[v, u, :]
 
-		#plt.show()
-
 		pixel_info["rgb"] = self.images[image_index][v, u, :]
-		#pixel_info["rgb2"] = g(self.images[image_index])[v, u, :]
 
 		if self.load_albedo:
 			pixel_info["albedo"] = self.albedos[image_index][v, u, :]
@@ -123,6 +121,9 @@ class NerfDataset(Dataset, ABC):
 			pixel_info["normal"] = self.normals[image_index][v, u, :]
 		if self.load_instance_label_mask:
 			pixel_info["label"] = self.masks[image_index][v, u]
+		if self.load_roughness:
+			pixel_info["roughness"] = self.roughness[image_index][v, u]
+
 		return pixel_info
 
 	def get_near_far_plane(self):
@@ -149,6 +150,8 @@ class NerfDataset(Dataset, ABC):
 				self.normals.append(data["normal"][0])
 			if self.load_albedo:
 				self.albedos.append(data["albedo"][0])
+			if self.load_roughness:
+				self.roughness.append(data["roughness"][0])
 		self.full_data_loaded = True
 
 	def to_tensor(self, device):
@@ -162,6 +165,8 @@ class NerfDataset(Dataset, ABC):
 			self.normals = torch.stack(self.normals, 0).to(device)
 		if self.load_albedo:
 			self.albedos = torch.stack(self.albedos, 0).to(device)
+		if self.load_roughness:
+			self.roughness = torch.stack(self.roughness, 0).to(device)
 
 	def __getitem__(self, item):
 		pass

src/nerf_models/nerf_decomp.py
@@ -67,18 +67,18 @@ class NeRFDecomp(nn.Module):
         self.radiance_linear = nn.Linear(W, 3)
 
         self.coarse_radiance_number = coarse_radiance_number
-
-        self.additional_radiance_linear_feature_1 = nn.Linear(W, W//2)
-        self.additional_radiance_linear_feature_2 = nn.Linear(W, W//2)
-        self.additional_radiance_linear_feature_3 = nn.Linear(W, W//2)
-
-        self.additional_radiance_linear_1 = nn.Linear(W//2, 3)
-        self.additional_radiance_linear_2 = nn.Linear(W//2, 3)
-        self.additional_radiance_linear_3 = nn.Linear(W//2, 3)
-
-        # self.additional_radiance_linear = nn.ModuleList(
-        #     [nn.Linear(W, 3)] * coarse_radiance_number
-        # )
+        self.additional_radiance_feature_linear = nn.ModuleList(
+            [nn.Linear(W, W // 2)] * coarse_radiance_number
+        )
+        self.additional_radiance_linear = nn.ModuleList(
+            [nn.Linear(W // 2, 3)] * coarse_radiance_number
+        )
+        #
+        # if coarse_radiance_number > 0:
+        #
+        # else:
+        #     self.additional_radiance_feature_linear = None
+        #     self.additional_radiance_linear = None
         #for i in range(self.coarse_radiance_number):
         #    self.additional_radiance_linear.append(nn.Linear(W, 3))
 
@@ -136,21 +136,15 @@ class NeRFDecomp(nn.Module):
             h = F.relu(h)
 
         radiance = self.radiance_linear(h)
-
-        radiance1 = self.additional_radiance_linear_feature_1(h)
-        radiance1 = F.relu(radiance1)
-        radiance1 = self.additional_radiance_linear_1(radiance1)
-
-        radiance2 = self.additional_radiance_linear_feature_2(h)
-        radiance2 = F.relu(radiance2)
-        radiance2 = self.additional_radiance_linear_2(radiance2)
-
-        radiance3 = self.additional_radiance_linear_feature_3(h)
-        radiance3 = F.relu(radiance3)
-        radiance3 = self.additional_radiance_linear_3(radiance3)
-
         ret = [sigma, albedo, roughness, irradiance, radiance]
-        ret += [radiance1, radiance2, radiance3]
+
+        for i in range(self.coarse_radiance_number):
+            radiance_i = self.additional_radiance_feature_linear[i](h)
+            radiance_i = F.relu(radiance_i)
+            radiance_i = self.additional_radiance_linear[i](radiance_i)
+            ret.append(radiance_i)
+
+        # ret += [radiance1, radiance2, radiance3]
 
         #for l in self.additional_radiance_linear:
         #    ret.append(l(h))

src/nerf_models/nerf_decomp_renderer.py
@@ -58,6 +58,8 @@ def raw2outputs(rays_o, rays_d, z_vals, z_vals_constant,
 				gt_values=None,
 				target_normal_map_for_radiance_calculation="ground_truth",
 				target_albedo_map_for_radiance_calculation="ground_truth",
+				target_roughness_map_for_radiance_calculation="ground_truth",
+				target_radiance_map_for_radiance_calculation="ground_truth",
 				**kwargs):
 	"""Transforms model's predictions to semantically meaningful values.
 	Args:
@@ -203,12 +205,16 @@ def raw2outputs(rays_o, rays_d, z_vals, z_vals_constant,
 	if target_albedo_map_for_radiance_calculation == "ground_truth":
 		target_albedo_map = gt_values["albedo"]
 
+	target_roughness_map = roughness_map
+	if target_roughness_map_for_radiance_calculation == "ground_truth":
+		target_roughness_map = gt_values["roughness"][...,0]
+
 	# (7) calculate color from split-sum approximation
-	n_dot_v = torch.sum(rays_d * target_normal_map, -1)
-	n_dot_v = F.relu(n_dot_v)
+	n_dot_v = torch.sum(-rays_d * target_normal_map, -1)
+	n_dot_v = torch.clip(n_dot_v, 0, 1)
 
 	# grid_sample input is  [-1, 1] x [-1, 1]
-	BRDF_2D_LUT_uv = torch.stack([2 * n_dot_v - 1, 2 * roughness_map - 1], -1)
+	BRDF_2D_LUT_uv = torch.stack([2 * n_dot_v - 1, 2 * target_roughness_map - 1], -1)
 	envBRDF = F.grid_sample(brdf_lut[None, ...], BRDF_2D_LUT_uv[None, :, None, ...], align_corners=True)
 	envBRDF = envBRDF.permute((0, 2, 3, 1))
 	envBRDF = envBRDF.squeeze()
@@ -216,12 +222,14 @@ def raw2outputs(rays_o, rays_d, z_vals, z_vals_constant,
 	# dielectric
 	F0 = torch.tensor([0.04, 0.04, 0.04])
 	F0 = F0.repeat(*depth_map.shape, 1)
+	target_metallic_map = (1-target_roughness_map)[...,None]
+	F0 = F0 * (1-target_metallic_map) + target_albedo_map * target_metallic_map
+
 	envBRDF_coefficient1 = envBRDF[..., 0]
 	envBRDF_coefficient0 = envBRDF[..., 1]
 	envBRDF_coefficient1 = torch.stack(3 * [envBRDF_coefficient1], -1)
-	fresnel_map = fresnel_schlick_roughness(n_dot_v, F0, roughness_map)
+	fresnel_map = fresnel_schlick_roughness(n_dot_v, F0, target_roughness_map)
 	specular_map = fresnel_map * envBRDF_coefficient1 + envBRDF_coefficient0[..., None]
-
 
 	with torch.no_grad():
 		reflected_dirs = rays_d - 2 * torch.sum(target_normal_map * rays_d, -1, keepdim=True) * target_normal_map
@@ -242,7 +250,7 @@ def raw2outputs(rays_o, rays_d, z_vals, z_vals_constant,
 		(1-mipmap_remainder) * prefiltered_env_maps[torch.arange(prefiltered_env_maps.size(0)), mipmap_index1] +\
 		mipmap_remainder * prefiltered_env_maps[torch.arange(prefiltered_env_maps.size(0)), mipmap_index2]
 
-	diffuse_map = (1 - fresnel_map) * target_albedo_map * irradiance_map[..., None]
+	diffuse_map = (1 - fresnel_map) * (1-target_metallic_map) * target_albedo_map * irradiance_map[..., None]
 	specular_map = specular_map * prefiltered_reflected_map
 	approximated_radiance_map = diffuse_map + specular_map
 
@@ -263,6 +271,7 @@ def raw2outputs(rays_o, rays_d, z_vals, z_vals_constant,
 	results["roughness_map"] = roughness_map
 	results["specular_map"] = specular_map
 	results["diffuse_map"] = diffuse_map
+	results["n_dot_v_map"] = n_dot_v
 
 	results["normal_map_from_sigma_gradient"] = normal_map_from_sigma_gradient
 	results["normal_map_from_sigma_gradient_surface"] = normal_map_from_sigma_gradient_surface
@@ -522,7 +531,7 @@ def render_decomp_path(dataset_test: NerfDataset, hwf, K, chunk, render_kwargs,
 
 		gt_values = dataset_test.get_resized_normal_albedo(render_factor, i)
 		for k in gt_values.keys():
-			gt_values[k] = torch.reshape(gt_values[k], [-1, 3])
+			gt_values[k] = torch.reshape(gt_values[k], [-1, gt_values[k].shape[-1]])
 		results_i = render_decomp(H, W, K, chunk=chunk, c2w=c2w[:3, :4], init_basecolor=init_basecolor, gt_values=gt_values, **render_kwargs)
 		append_result(results_i, "color_map", i, "rgb")
 		append_result(results_i, "radiance_map", i, "radiance")
@@ -535,6 +544,7 @@ def render_decomp_path(dataset_test: NerfDataset, hwf, K, chunk, render_kwargs,
 		append_result(results_i, "roughness_map", i, "roughness")
 		append_result(results_i, "specular_map", i, "specular")
 		append_result(results_i, "diffuse_map", i, "diffuse")
+		append_result(results_i, "n_dot_v_map", i, "n_dot_v")
 
 		append_result(results_i, "normal_map_from_sigma_gradient", i, "normal_map_from_sigma_gradient")
 		append_result(results_i, "normal_map_from_sigma_gradient_surface", i, "normal_map_from_sigma_gradient_surface")

src/run_nerf_decomp.py
@@ -2,7 +2,7 @@
 #
 # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 # os.environ["CUDA_VISIBLE_DEVICES"] = str(5)
-#
+
 
 import numpy as np
 import torch
@@ -27,6 +27,7 @@ from utils.generator_utils import *
 from utils.timing_utils import *
 import cv2
 from torch.nn.functional import normalize
+
 
 def test():
     raise NotImplementedError
@@ -117,6 +118,7 @@ def train():
         load_params = {
             "image_scale": args.image_scale,
             "load_normal": True, #args.learn_normal_from_oracle,
+            "load_roughness": True,
             "load_albedo": args.learn_albedo_from_oracle,
             "sample_length": args.sample_length,
             "coarse_radiance_number": args.coarse_radiance_number
@@ -339,12 +341,6 @@ def train():
             return loss_from_target
 
         # 2. calculate loss
-        # 1) rendering loss
-        # rgb = result['color_map']
-        # loss_render = mse_loss(rgb, target_rgb)
-        # if 'color_map0' in result:
-        #     loss_render0 = mse_loss(result['color_map0'], target_rgb)
-        #     loss_render = loss_render + loss_render0
 
         # 0) approximated radiance loss
         loss_render = calculate_loss("color_map", target_rgb)
@@ -416,6 +412,8 @@ def train():
                 writer.add_scalar('Loss_normal/inferred_normal', loss_inferred_normal, i)
                 loss_from_gt = calculate_loss("inferred_normal_map", "ground_truth_normal")
                 writer.add_scalar('Loss_normal/inferred_normal_from_gt', loss_from_gt, i)
+
+        # total_loss = args.beta_radiance_render * loss_render_radiance
 
         total_loss.backward()
         optimizer.step()