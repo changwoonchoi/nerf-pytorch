f4915dada9617b3b7d88ef6e78cc9cb6169788c9
juhyeonkim95 <cjdeka3123@snu.ac.kr>
2021-12-31 13:42:12 +0900

    20211231 monte carlo integration implemented

--- src/run_nerf_decomp.py
+++ src/run_nerf_decomp.py
@@ -1,7 +1,7 @@
-import os
-
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = str(2)
+# import os
+#
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = str(2)
 #import matplotlib
 #matplotlib.use('TkAgg')
 
